69bf58a5ce8619452ab5b8c9d7e30bc54269185e
Shimpei Otsubo <potsbo@gmail.com>
2016-07-18 18:14:40 +0900

    implementation for JAN

--- lib/ean_decoder.rb
+++ lib/ean_decoder.rb
@@ -2,7 +2,16 @@ require "ean_decoder/version"
 
 module EanDecoder
   def country
-    'jp'
+    'jp' if ['45','49'].include? self[0..1]
+  end
+
+  def manufacturer_code
+    return unless country == 'jp'
+    digs = case self[0...3]
+             when '456','457','458','459' then 9
+             else 7
+           end
+    self[0...digs]
   end
 end
 
--- spec/ean_decoder_spec.rb
+++ spec/ean_decoder_spec.rb
@@ -6,6 +6,27 @@ describe EanDecoder do
   end
 
   it 'should identify Japan' do
-    expect('1234567890123'.country).to eq('jp')
+    expect('4534567890123'.country).to eq('jp')
+    expect('4934567890123'.country).to eq('jp')
+    expect('5534567890123'.country).not_to eq('jp')
+    expect('5934567890123'.country).not_to eq('jp')
+  end
+
+  it 'should respond to both 7 digits manufacturer codes' do
+    base = '1234567890123'
+    rest = base[3...13]
+    manu_rest = base[3...7]
+    ((450..455).to_a + (490..499).to_a).each do |code|
+      expect((code.to_s + rest).manufacturer_code).to eq(code.to_s + manu_rest)
+    end
+  end
+
+  it 'should respond to both 9 digits manufacturer codes' do
+    base = '1234567890123'
+    rest = base[3...13]
+    manu_rest = base[3...9]
+    (456..459).to_a.each do |code|
+      expect((code.to_s + rest).manufacturer_code).to eq(code.to_s + manu_rest)
+    end
   end
 end
